5d34bfffc1f20ef9a66f631e51a025b35c68f15f
youngjin-korea <youngjin1293@naver.com>
2023-10-14 10:34:09 +0900

    first

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,75 +1,64 @@
 import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 
-const LogIn = () => {
-  const [loginData, setLoginData] = useState({
-    email: "",
-    password: "",
-  });
-  console.log("email:", loginData.email);
-  console.log("pw:", loginData.password);
-  // input tag에서 onchange에 호출될 함수 (객체 변수 e 에서 TS7006: Parameter 'e' implicitly has an 'any' type. 에러 발생)
-  // const changeHandle = (e) => {
-  //   setLoginData({ ...loginData, [e.target.name]: e.target.value });
-  // };
+function Login() {
+  const navigate = useNavigate();
+  const [loginData, setLoginData] = useState({ email: "", password: "" });
 
-  // 로그인 버튼을 눌렀을 때 AXIOS 통신으로 back-end에 아이디 비번 전송
-  const submitHandle = async () => {
-    await axios
-      .post("https://your-api-url.com/login", loginData)
-      .then((response) => {
-        // 로그인 성공 시, 본인 페이지로 이동
-        if (response.data.success) {
-          console.log("로그인 성공!");
-          const { accessToken, refreshToken } = response.data; // 서버 응답에서 토큰 추출
-          // Access Token과 Refresh Token을 localStorage에 저장
-          localStorage.setItem("accessToken", accessToken);
-          localStorage.setItem("refreshToken", refreshToken);
-          // 페이지 이동 로직 구현
-        } else {
-          // 다시 로그인 페이지로 이동
-        }
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(
+        "https://your-api-url.com/login",
+        loginData
+      );
+      // response 확인용 출력(확인 후 삭제)
+      console.log(`response.data:${response.data}`);
 
-        // 개인 페이지로 이동시켜 주기!
-      })
-      .catch((error) => {
-        console.error("로그인 실패:", error);
-      });
-
-    // 로그인 완료, 에러 후 스테이트 초기화
+      if (response.data) {
+        console.log("로그인 성공!");
+        const { accessToken, refreshToken } = response.data;
+        localStorage.setItem("accessToken", accessToken);
+        localStorage.setItem("refreshToken", refreshToken);
+        console.log(`accessToken:${accessToken},refreshToken:${refreshToken}`); //테스트용 출력
+        navigate("/page/:userId"); // 로그인 성공 시, 개인 페이지로 이동
+      } else {
+        console.log("로그인 실패");
+        navigate(-1);
+      }
+    } catch (error) {
+      console.error("로그인 실패:", error);
+      // if (error.response && error.response.status === 401) {
+      //   alert("인증 실패: 올바른 사용자 이름과 암호를 입력하십시오.");
+      // } else {
+      //   alert("로그인에 실패했습니다. 다시 시도하십시오.");
+      // }
+    }
     setLoginData({ email: "", password: "" });
   };
 
-  // 나중에 토큰을 사용할 때는 localStorage에서 가져올 수 있습니다.
-  const storedAccessToken = localStorage.getItem("accessToken");
-  const storedRefreshToken = localStorage.getItem("refreshToken");
-
   return (
-    <div id="wrapper">
-      <div id="inputEmail">
-        <input
-          name="email"
-          type="text"
-          value={loginData.email}
-          onChange={(e) => {
-            setLoginData({ ...loginData, [e.target.name]: e.target.value });
-          }}
-        />
-      </div>
-      <div id="inputPassword">
-        <input
-          name="password"
-          type="password"
-          value={loginData.password}
-          onChange={(e) => {
-            setLoginData({ ...loginData, [e.target.name]: e.target.value });
-          }}
-        />
-      </div>
-
-      <button onClick={submitHandle}>로그인</button>
+    <div>
+      <input
+        type="text"
+        placeholder="이메일"
+        value={loginData.email}
+        onChange={(e) => setLoginData({ ...loginData, email: e.target.value })}
+      />
+      <input
+        type="password"
+        placeholder="비밀번호"
+        value={loginData.password}
+        onChange={(e) =>
+          setLoginData({ ...loginData, password: e.target.value })
+        }
+      />
+      <button onClick={handleSubmit}>로그인</button>
+      <Link to={"/signin"}>
+        <button>회원가입</button>
+      </Link>
     </div>
   );
-};
+}
 
-export default LogIn;
+export default Login;
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,63 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+
 const SignIn = () => {
-  return <div>회원가입 페이지 입니다.</div>;
+  const navigate = useNavigate();
+  const [signinData, setSigninData] = useState({
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    nickname: "",
+  });
+  const [passwordMatch, setPasswordMatch] = useState(true);
+  const handleSubmit = async () => {
+    if (signinData.password === signinData.passwordConfirm) {
+      try {
+        const response = await axios.post("https://api.example.com/register", {
+          email: signinData.email,
+          password: signinData.password,
+          nickname: signinData.nickname,
+        });
+      } catch (error) {
+        console.error("회원가입 실패", error);
+      }
+    } else {
+      setPasswordMatch(false);
+    }
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="이메일"
+        value={signinData.email}
+        onChange={(e) =>
+          setSigninData({ ...signinData, email: e.target.value })
+        }
+      />
+      <input
+        type="password"
+        placeholder="비밀번호"
+        value={signinData.password}
+        onChange={(e) =>
+          setSigninData({ ...signinData, password: e.target.value })
+        }
+      />
+      <input
+        type="password"
+        placeholder="비밀번호 확인"
+        value={signinData.passwordConfirm}
+        onChange={(e) =>
+          setSigninData({ ...signinData, passwordConfirm: e.target.value })
+        }
+      />
+      {!passwordMatch && (
+        <p style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</p>
+      )}
+      <button onClick={handleSubmit}>가입하기</button>
+    </div>
+  );
 };
 
 export default SignIn;
